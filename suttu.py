d0150376a13f293f383d4cc84cd61ef694f6170a
Mika Vainio <koti.mika.vainio@gmail.com>
2021-02-18 18:34:47 +0200

    Lisätty vuokaavio

--- suttu.py
+++ suttu.py
@@ -1,15 +1,16 @@
 # Testataan merkkijonoa numeroarvo
 numeroarvo = '123.45'
+print('Tutkittava numeerinen merkkijono on:', numeroarvo)
 
 # Etsitään desimaalipisteen paikka merkkijonosta (.)
 pilkunpaikka = numeroarvo.find('.')
-print(pilkunpaikka)
+print('Pilkku löytyy indeksistä:',pilkunpaikka, 'ja se on', pilkunpaikka + 1, 'merkki vasemmalta')
 
 # Pilkotaan merkkijono pisteen kohdalta
 osat = numeroarvo.split('.')
 
 # Selvitetään len-funktiolla montako osaa tuli: 1 pilkku -> 2 osaa. Jos enemmän osia -> virhe
-print(osat, 'osia on', len(osat), 'kpl')
+print('Merkkijonon osat ovat' ,osat ,'ja niitä on', len(osat), 'kpl')
 
 # Tietotyyppi on split-metodin ansiosta nyt merkkijonon sijaan lista,
 # jossa ei ole isnumeric-metodia. Siksi muutetaan alkiot merkkijonoiksi
