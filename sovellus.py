622b866022857288337af0bbd597d719cab0339d
Mika Vainio <koti.mika.vainio@gmail.com>
2021-03-04 15:07:33 +0200

    Päivitetty pääohjelmaa ja laskentaa

--- sovellus.py
+++ sovellus.py
@@ -1,25 +1,34 @@
 # Tämä on painonhallintasovelluksen pääohjelma
 
 # Kirjastojen ja modulien käyttöönotot
-import sanity
-from laskenta import *
+import sanity2
+import laskenta
 
 # Varsinaisen pääohjelman alku
-
-# Komponenttien alustukset
 
 # Työsilmukka, ikuinen silmukka, jossa on poistumistoiminto (ehto True on aina voimassa)
 uusi = 'K'
 while True:
 
     # Kysytään käyttäjältä paino
-    paino_str = input('Paino (kg)? ')
-    paino = sanity.on_jarkeva(paino_str, 20, 300)
-
+    tapahtui_virhe = True
+    while tapahtui_virhe == True:
+        paino_str = input('Paino (kg)? ')
+        tulokset = sanity2.liukuluvuksi(paino_str)
+        if tulokset[0] == 0:
+            paino = tulokset[2]
+            tapahtui_virhe = False
+        else:
+            print(tulokset[1])
+    # Testi
+    print('Ja paino oli', paino, 'kg')
+              
+    '''
     pituus_str = input('Pituus (m)? ')
-    pituus = sanity.on_jarkeva(pituus_str, 1, 3)
+    pituus = 
 
     print('Painoindeksisi on', bmi(paino, pituus))
+    '''
     # Poistuminen ikuisesta silmukasta
     uusi = input('Lasketaanko uuden henkilön rasvaprosentti? (K/E)')
     if uusi == 'E':
