a4dc5c6085bfa88bfce40d3cef9b5fa70db4f650
Mika Vainio <koti.mika.vainio@gmail.com>
2021-03-17 13:39:41 +0200

    Kirjoitettu lisää testejä

--- test_laskenta.py
+++ test_laskenta.py
@@ -5,5 +5,5 @@ import laskenta
 
 # Ensimmäinen testi lasketaan painoindeksi ja verrataan tulosta ennalta laskettuun arvoon
 def test_painoideksi():
-    assert laskenta.bmi(74, 1.71) == 20
+    assert laskenta.bmi(74, 1.71) == 25.3069320474676
     
