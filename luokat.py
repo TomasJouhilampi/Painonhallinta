16009c7a3161f8c0703b2030af98300e85db310b
Tomas <43516@edu.raseko.fi>
2021-03-25 09:45:17 +0200

    Update luokat.py

--- luokat.py
+++ luokat.py
@@ -1,17 +1,3 @@
-<<<<<<< Updated upstream
-# Tässä modulissa määritellään luokat painonhallintasovellukseen
-
-# Modulien ja kirjastojen lataukset
-
-
-# Henkilö-luokka
-
-class Henkilo:
-    """Yliluokka kaikille henkilötyypeille"""
-    def __init__(self, etunimi, sukunimi, pituus, paino, ika, sukupuoli):
-        
-        self.etunimi = etunimi
-=======
 # Tässä modulissa määritellään luokat painon hallinta sovellukseen
 
 # Modulien ja kirjastojen sovellukset
@@ -24,7 +10,6 @@ class ClassName(object):
     def __init__(self, etunimi, sukunimi, pituus, paino, ika, sukupuoli):
 
         self.etumimi = etunimi
->>>>>>> Stashed changes
         self.sukunimi = sukunimi
         self.pituus = pituus
         self.paino = paino
@@ -33,51 +18,6 @@ class ClassName(object):
 
     def painoindeksi(self):
         bmi = self.paino / (self.pituus / 100) ** 2
-<<<<<<< Updated upstream
-        return bmi
-
-    @staticmethod
-    def bmi(pituus, paino):
-        bmi = paino / (pituus/100)**2
-        return bmi
-
-class Aikuinen(Henkilo):
-    """Aliluokka aikuiselle henkilölle, perii Henkilo-luokan ominaisuudet
-    ja metodit
-    """
-    def __init__(self, etunimi, sukunimi, pituus, paino, ika, sukupuoli, tavoitepaino):
-        super().__init__(etunimi, sukunimi, pituus, paino, ika, sukupuoli)
-        self.tavoitepaino = tavoitepaino
-
-    def rasvaprosentti(self):
-        
-        rasvaprosentti = 1.2 * self.painoindeksi() + 0.23 * self.ika - 10.8 * self.sukupuoli - 5.4
-        return rasvaprosentti
-
-
-
-
-if __name__ == "__main__":
-    mikaV = Henkilo('Mika', 'Vainio', 171, 74, 59, 1)
-    print('Henkilö painaa', mikaV.paino)
-
-    mikaV.painoindeksi()
-
-    mikaV2 = Aikuinen('Mika', 'Vainio', 171, 74, 59, 1, 70)
-    print(mikaV2.etunimi, 'painoindeksi', mikaV2.painoindeksi())
-    print(mikaV2.etunimi, 'rasvaprosentti', mikaV2.rasvaprosentti())
-
-    # Lasketaan painoindeksi staattisella metodilla
-    pituus = 171
-    paino = 75
-
-    print('Paino indeksi on ', Henkilo.bmi(pituus, paino))
-
-
-    
-        
-
-=======
         print("paino indeksisi on", bmi)
 
 class Aikuinen(Henkilo):
@@ -91,6 +31,8 @@ def rasvaprosentti(self):
     rasvaprosentti = 1.2 * bmi + 0.23 * self.ika - 10.8 * self.sukupuoli
     return rasvaprosentti
 
+class classname(object):
+    ""docstring for classname""
 
 
 if __name__ == "__name__":
@@ -104,4 +46,3 @@ if __name__ == "__name__":
     print (MikaV2.etunimi, "painoindeksi", MikaV2.painoindeksi())
     print (MikaV2.etunimi, "rasvaprosentti", MikaV2.rasvaprosentti())
     
->>>>>>> Stashed changes
